e1bac2649d98dcf493fa727a53147db051be374e
Shubham <shubhampark007@gmail.com>
2024-08-02 12:02:18 +0530

    added in branch1

---
+++ Student.java
@@ -0,0 +1,5 @@
+public class Student {
+    public static void main(String[] args) {
+        System.err.println("Welcome to the Student");
+    }
+}
--- hello.java
+++ hello.java
@@ -3,6 +3,7 @@ public static void main(String[] args) {
     System.err.println("Hellow World");
 }
 
+//----------------------------------------------------------------
 public int addn(int x, int y, int z){
     return x + y + z;
 }
